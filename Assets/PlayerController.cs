bcd2361ab3c40486281778d88702de978fc8d6fd
Rynndi <Catherinedi2020@gmail.com>
2026-03-05 15:40:33 -0800

    fixed merge conflict with ink's branch, pushing to main

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    float movementX;
+    float movementY;
+    [SerializeField] float speed = 5.0f;
+    Rigidbody2D rb;
+    int score = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        // animator = GetComponent<Animator>();
+        // spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 v = value.Get<Vector2>();
+        movementX = v.x;
+        movementY = v.y;
+        Debug.Log("Movement X = " + movementX);
+        Debug.Log("Movement Y = " + movementY);
+    }
+
+    void FixedUpdate()
+    {
+        float movementDistanceX = movementX * speed * Time.deltaTime;
+        float movementDistanceY = movementY * speed * Time.deltaTime;
+        transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);
+    }
+    // Update is called once per frame
+    
+    void Update()
+    {
+        
+    }
+}
